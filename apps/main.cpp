6dc61028e0dafb1875fbc3ce677d1aa4ab907cdf
Мельников Вадим Сергеевич <vasemelnikov@edu.hse.ru>
2025-02-27 23:04:17 +0300

    improve ux

--- apps/main.cpp
+++ apps/main.cpp
@@ -2,12 +2,61 @@
 #include <parser/parser.h>
 #include <parser/tokenizer.h>
 
+#include <cstring>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 void usage() {
-    std::cout << "Usage: beautify read_from [write_to]\n";
-    std::cout << "If write_to is not specified, a beautified file is printed to stdout.\n";
+    std::cout << "Usage: ./beautify read_from [write_to] [OPTIONS]\n";
+    std::cout << "\n";
+    std::cout << "Description: this program accepts a file as input and outputs the same file but formatted either to "
+                 "another file or stdout.\n";
+    std::cout << "\n";
+    std::cout << "Options:\n";
+    std::cout << "  --help                         Shows this message\n";
+    std::cout << "  --spaces-per-tab -t            Specifies amount of spaces a tab should be expanded as "
+                 "(defaults to 8)";
+}
+
+// Returns an object of structure {{input filename, output filename (stdout if empty)}, spaces per tab}
+std::pair<std::pair<std::string, std::string>, size_t> ParseArgs(int argc, char* argv[]) {
+    std::string in_filename = argv[1];
+    std::string out_filename;
+    size_t spaces = 8;
+    for (int i = 0; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "--help") {
+            usage();
+            exit(0);
+        } else if (arg == "--spaces-per-tab" || arg == "-t") {
+            if (i + 1 < argc) {
+                try {
+                    spaces = std::stoull(argv[i + 1]);
+                } catch (const std::invalid_argument&) {
+                    std::cerr << "Invalid value for spaces per tab argument: " << argv[i + 1] << ".\n";
+                    exit(1);
+                }
+                ++i;
+            } else {
+                std::cerr << "No spaces per tab argument value was provided.\n";
+                exit(1);
+            }
+        } else if (in_filename.empty()) {
+            in_filename = arg;
+        } else if (out_filename.empty()) {
+            out_filename = arg;
+        } else {
+            std::cerr << "Unknown argument: " << arg << ".\n";
+            exit(1);
+        }
+    }
+    if (in_filename.empty()) {
+        std::cerr << "No input filename was provided.\n";
+        exit(1);
+    }
+    return {{in_filename, out_filename}, spaces};
 }
 
 int main(int argc, char* argv[]) {
@@ -15,21 +64,21 @@ int main(int argc, char* argv[]) {
         usage();
         return 0;
     }
-    std::string filename = argv[1];
-    std::ifstream in(filename);
+    auto [filenames, spaces] = ParseArgs(argc, argv);
+    auto& [in_filename, out_filename] = filenames;
+    std::ifstream in(in_filename);
     if (in.fail()) {
-        std::cerr << "File `" << filename << "` does not exist.\n";
+        std::cerr << "File `" << in_filename << "` does not exist.\n";
         return 1;
     }
     Tokenizer tokenizer(&in);
     Parser parser(tokenizer);
     Module file = parser.ParseModule();
-    if (argc == 2) {
+    if (out_filename.empty()) {
         CodeGenerator gen(std::cout);
         gen.Generate(file);
     } else {
-        std::string write_to = argv[2];
-        std::ofstream out(write_to);
+        std::ofstream out(out_filename);
         CodeGenerator gen(out);
         gen.Generate(file);
     }
